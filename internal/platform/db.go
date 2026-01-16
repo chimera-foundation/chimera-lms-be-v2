e377e9c6b82b8483208593e6423f72be20472bb6
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-16 23:06:17 +0700

    refactor(db): update comments for debugging in NewPostgresDB function

--- internal/platform/db.go
+++ internal/platform/db.go
@@ -5,10 +5,13 @@ import (
 	"github.com/uptrace/bun"
 	pgdialect "github.com/uptrace/bun/dialect/sqlitedialect"
 	"github.com/uptrace/bun/driver/pgdriver"
+	// "github.com/uptrace/bun/extra/bundebug" // You might need to go get this
 )
 
 func NewPostgresDB(dsn string) *bun.DB {
 	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
 	db := bun.NewDB(sqldb, pgdialect.New())
+	// uncomment to debug query
+	// db.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(true)))
 	return db
 }
