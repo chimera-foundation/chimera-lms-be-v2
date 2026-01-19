876fecb2ca61d2cff3cb61e1c931baee53ed82b9
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-19 20:49:00 +0700

    feat(model): add OrgType enumeration and update Organization struct with Type and Address fields

--- internal/features/organization/domain/organization.go
+++ internal/features/organization/domain/organization.go
@@ -4,10 +4,22 @@ import (
 	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
 )
 
+type OrgType int
+
+const (
+	University OrgType = iota
+	HighSchool
+	MiddleSchool
+	GradeSchool
+)
+
 type Organization struct {
 	shared.Base
 	
 	Name string `validate:"required,min=3,max=100"`
 	Slug string `validate:"required,alphanum"`
+	Type OrgType `validate:"required"`
+	Address string `validate:"required,min=10,max=200"`
+	
 	IsActive bool
 }
