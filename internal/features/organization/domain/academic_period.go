fdd11d05da829a80ff21b3e20a6e288d77efe955
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-20 19:52:09 +0700

    feat(domain): moved AcademicPeriod struct for managing academic periods

--- internal/features/organization/domain/academic_period.go
+++ internal/features/organization/domain/academic_period.go
@@ -4,8 +4,6 @@ import (
 	"time"
 
 	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
-	co "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/cohort/domain"
-	e "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/enrollment/domain"
 )
 
 type AcademicPeriod struct {
@@ -14,8 +12,6 @@ type AcademicPeriod struct {
 	Name string // e.g., "2025/2026 ganjil"
 	StartDate time.Time
 	EndDate time.Time
-	Cohorts []co.Cohort
-	Enrollments []e.Enrollment
 
 	IsActive bool
 }
