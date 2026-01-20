cdaa875a9b55f4b98c9cef1012add68653b61455
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-20 17:32:08 +0700

    feat(domain): add Contents field to Lesson struct for content management

--- internal/features/lesson/domain/lesson.go
+++ internal/features/lesson/domain/lesson.go
@@ -1,10 +1,14 @@
 package domain
 
-import "github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
+import (
+	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
+	con "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/content/domain"
+)
 
 type Lesson struct {
 	shared.Base
 
 	Title string
 	OrderIndex int
+	Contents []con.Content
 }
