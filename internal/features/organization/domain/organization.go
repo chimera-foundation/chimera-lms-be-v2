5d08158ea1738ebae39d0dc4f141554e32b2f124
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-20 19:56:09 +0700

    refactor(domain): remove unused fields from Organization struct

--- internal/features/organization/domain/organization.go
+++ internal/features/organization/domain/organization.go
@@ -1,13 +1,6 @@
 package domain
 
 import (
-	u "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/user/domain"
-	ap "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/academic_period/domain"
-	p "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/program/domain"
-	c "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/course/domain"
-	co "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/cohort/domain"
-	e "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/education_level/domain"
-	s "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/subject/domain"
 	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
 )
 
@@ -27,13 +20,7 @@ type Organization struct {
 	Slug string `validate:"required,alphanum"`
 	Type OrgType `validate:"required"`
 	Address string `validate:"required,min=10,max=200"`
-	Users []u.User 
-	AcademicPeriods []ap.AcademicPeriod
-	Programs []p.Program
-	Courses []c.Course
-	Cohorts []co.Cohort
-	EducationLevels []e.EducationLevel
-	Subjects []s.Subject
+	AcademicPeriods []AcademicPeriod
 
 	IsActive bool
 }
