99f23649f4ccb7b351dc573c7d3a67267ca633ed
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-20 16:10:25 +0700

    feat(domain): add Sections field to Cohort struct

--- internal/features/cohort/domain/cohort.go
+++ internal/features/cohort/domain/cohort.go
@@ -2,10 +2,12 @@ package domain
 
 import (
 	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
+	"github.com/chimera-foundation/chimera-lms-be-v2/internal/features/section/domain"
 )
 
 type Cohort struct {
 	shared.Base
 
 	Name string
+	Sections []domain.Section
 }
