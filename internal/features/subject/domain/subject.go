f6a60d4760c6354dd06b66da69a50b6a60beb8b1
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-20 16:59:17 +0700

    feat(domain): add Courses field to Subject struct for course associations

--- internal/features/subject/domain/subject.go
+++ internal/features/subject/domain/subject.go
@@ -1,10 +1,14 @@
 package domain
 
-import "github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
+import (
+	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
+	"github.com/chimera-foundation/chimera-lms-be-v2/internal/features/course/domain"
+)
 
 type Subject struct {
 	shared.Base
 
 	Name string
 	Code string
+	Courses []domain.Course
 }
