94819662fb3e302935fc8847872257137f69c36d
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-20 17:09:19 +0700

    feat(domain): define SectionRoleType and update Section struct to include Users

--- internal/features/section/domain/section.go
+++ internal/features/section/domain/section.go
@@ -2,6 +2,16 @@ package domain
 
 import (
 	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
+	u "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/user/domain"
+)
+
+type SectionRoleType int
+
+const (
+	Student SectionRoleType = iota
+	Teacher
+	Assistant
+	Monitor
 )
 
 type Section struct {
@@ -10,4 +20,6 @@ type Section struct {
 	Name string
 	RoomCode string
 	Capacity int
+
+	Users []u.User
 }
