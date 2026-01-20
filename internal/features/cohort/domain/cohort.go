d6c03964a2877983db262d275a27f7a27f9e66f0
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-20 19:54:20 +0700

    feat(domain): add OrganizationID, AcademicPeriodID, and EducationLevelID fields to Cohort struct

--- internal/features/cohort/domain/cohort.go
+++ internal/features/cohort/domain/cohort.go
@@ -2,12 +2,15 @@ package domain
 
 import (
 	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
-	"github.com/chimera-foundation/chimera-lms-be-v2/internal/features/section/domain"
+	"github.com/google/uuid"
 )
 
 type Cohort struct {
 	shared.Base
 
+	OrganizationID uuid.UUID
+	AcademicPeriodID uuid.UUID
+	EducationLevelID uuid.UUID
+
 	Name string
-	Sections []domain.Section
 }
