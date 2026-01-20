ad00ec71edcfca15633a725024ffd5606e97615e
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-20 19:56:39 +0700

    feat(domain): add EnrollmentID and ContentID fields to ProgressTracker struct

--- internal/features/progress_tracker/domain/progress_tracker.go
+++ internal/features/progress_tracker/domain/progress_tracker.go
@@ -4,10 +4,14 @@ import (
 	"time"
 
 	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
+	"github.com/google/uuid"
 )
 
 type ProgressTracker struct {
 	shared.Base
+
+	EnrollmentID uuid.UUID
+	ContentID uuid.UUID
 
 	IsCompleted bool
 	UpdatedAt time.Time
