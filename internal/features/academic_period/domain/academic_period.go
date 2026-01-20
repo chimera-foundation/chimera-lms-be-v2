ee6e087a5091cfed3d3fadd7dfc71c5ec6ce97b0
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-20 15:52:20 +0700

    feat(domain): add Cohorts field to AcademicPeriod struct

--- internal/features/academic_period/domain/academic_period.go
+++ internal/features/academic_period/domain/academic_period.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
+	"github.com/chimera-foundation/chimera-lms-be-v2/internal/features/cohort/domain"
 )
 
 type AcademicPeriod struct {
@@ -12,6 +13,7 @@ type AcademicPeriod struct {
 	Name string // e.g., "2025/2026 ganjil"
 	StartDate time.Time
 	EndDate time.Time
+	Cohorts []domain.Cohort
 
 	IsActive bool
 }
