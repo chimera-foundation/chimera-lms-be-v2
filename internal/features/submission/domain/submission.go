625910054f70d873cb106386591c51c2d97a72b5
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-20 19:58:41 +0700

    feat(domain): add AssessmentID, UserID, and EnrollmentID fields to Submission struct

--- internal/features/submission/domain/submission.go
+++ internal/features/submission/domain/submission.go
@@ -4,10 +4,15 @@ import (
 	"time"
 
 	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
+	"github.com/google/uuid"
 )
 
 type Submission struct {
 	shared.Base
+
+	AssessmentID uuid.UUID
+	UserID uuid.UUID
+	EnrollmentID uuid.UUID
 
 	FinalScore float32
 	SubmittedAt time.Time
