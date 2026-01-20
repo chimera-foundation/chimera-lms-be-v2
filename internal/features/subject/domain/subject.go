9a9500f68977b5dc1850af2638f099071c2aa23c
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-20 19:58:23 +0700

    feat(domain): add EducationLevelID and OrganizationID field to Subject struct

--- internal/features/subject/domain/subject.go
+++ internal/features/subject/domain/subject.go
@@ -2,13 +2,15 @@ package domain
 
 import (
 	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
-	"github.com/chimera-foundation/chimera-lms-be-v2/internal/features/course/domain"
+	"github.com/google/uuid"
 )
 
 type Subject struct {
 	shared.Base
 
+	OrganizationID uuid.UUID
+	EducationLevelID uuid.UUID
+
 	Name string
 	Code string
-	Courses []domain.Course
 }
