24f4c8caba3e2d8c57c05373faf1eb2b48c96d8e
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-22 17:05:35 +0700

    fix(dto): change OrganizationID type from UUID to string in RegisterRequest

--- internal/features/user/delivery/dto/auth.go
+++ internal/features/user/delivery/dto/auth.go
@@ -1,16 +1,14 @@
 package dto
 
-import "github.com/google/uuid"
-
 type RegisterRequest struct {
 	Email          string    `json:"email"`
 	Password       string    `json:"password"`
 	FirstName      string    `json:"first_name"`
 	LastName       string    `json:"last_name"`
-	OrganizationID uuid.UUID `json:"organization_id"`
+	OrganizationID string `json:"organization_id"`
 }
 
 type LoginRequest struct {
-        Email    string `json:"email"`
-        Password string `json:"password"`
-    }
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
