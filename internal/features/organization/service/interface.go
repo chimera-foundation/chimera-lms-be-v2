a3ec7ee12bb3f92528031ee7f8c116cfb8157de0
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-22 22:15:58 +0700

    feat(organization): add CRUD interface for organization management

---
+++ internal/features/organization/service/interface.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"context"
+
+	"github.com/chimera-foundation/chimera-lms-be-v2/internal/features/organization/domain"
+	"github.com/google/uuid"
+)
+
+type CRUD interface {
+	RegisterOrganization(ctx context.Context, name, slug, address string, isActive bool) (*domain.Organization, error)
+	DeleteOrganization(ctx context.Context, orgID uuid.UUID) (error) 
+	GetOrganization(ctx context.Context, orgID uuid.UUID) (*domain.Organization, error)
+	GetAllOrganizations(ctx context.Context)
+}
