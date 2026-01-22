2d38441c533fc1f53bd91e3780e6846676a6f04a
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-22 13:44:58 +0700

    feat(base): change CreatedBy and UpdatedBy fields to pointers for nullable support

--- internal/shared/base.go
+++ internal/shared/base.go
@@ -10,11 +10,22 @@ type Base struct {
 	
 	// Audit metada
 	CreatedAt time.Time
-	CreatedBy uuid.UUID
+	CreatedBy *uuid.UUID
 	UpdatedAt time.Time
-	UpdatedBy uuid.UUID
+	UpdatedBy *uuid.UUID
 
 	// Optional/nullable fields
 	DeletedAt *time.Time
 	DeletedBy *uuid.UUID
+}
+
+func (b *Base) PrepareCreate(actorID *uuid.UUID) {
+    if b.ID == uuid.Nil {
+        b.ID = uuid.New()
+    }
+    now := time.Now()
+    b.CreatedAt = now
+    b.UpdatedAt = now
+    b.CreatedBy = actorID
+    b.UpdatedBy = actorID
 }
