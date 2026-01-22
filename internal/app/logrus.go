1d8d9e42c7d2bbe0107b83b413cd7ec245bb0008
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-22 16:54:57 +0700

    feat(logger): add NewLogger function for logrus integration with configurable log level

---
+++ internal/app/logrus.go
@@ -0,0 +1,15 @@
+package app
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func NewLogger(viper *viper.Viper) *logrus.Logger {
+	log := logrus.New()
+
+	log.SetLevel(logrus.Level(viper.GetInt("LOG_LEVEL")))
+	log.SetFormatter(&logrus.JSONFormatter{})
+
+	return log
+}
