b11299bd7e513fed807d9254e3129713e02c9be4
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-24 21:30:57 +0700

    feat(section): add GetByID method to SectionRepository interface

--- internal/features/section/domain/section_repo.go
+++ internal/features/section/domain/section_repo.go
@@ -7,4 +7,5 @@ import (
 
 type SectionRepository interface {
     GetSectionIDsByUserID(ctx context.Context, userID uuid.UUID) ([]uuid.UUID, error)
+    GetByID(ctx context.Context, id uuid.UUID) (*Section, error)
 }
