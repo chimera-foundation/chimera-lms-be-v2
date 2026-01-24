a8a307209f8497e7f6bc08183aee149a2d96aa85
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-24 21:03:57 +0700

    feat(cohort): add CohortMember struct for cohort membership representation

---
+++ internal/features/cohort/domain/cohort_member.go
@@ -0,0 +1,13 @@
+package domain
+
+import (
+	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
+	"github.com/google/uuid"
+)
+
+type CohortMember struct {
+	shared.Base
+
+	CohortID uuid.UUID
+	UserID uuid.UUID
+}
