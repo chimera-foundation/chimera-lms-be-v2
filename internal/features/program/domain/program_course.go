80c6a56588f8abd850b94fade9288e1539acbcc5
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-27 21:20:53 +0700

    refactor(program_course): remove unused shared.Base field from ProgramCourse struct

--- internal/features/program/domain/program_course.go
+++ internal/features/program/domain/program_course.go
@@ -1,14 +1,11 @@
 package domain
 
 import (
-	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared"
 	"github.com/google/uuid"
 )
 
 type ProgramCourse struct {
-	shared.Base
-
-	ProgramID uuid.UUID
-	CourseID uuid.UUID
+	ProgramID  uuid.UUID
+	CourseID   uuid.UUID
 	OrderIndex int
 }
