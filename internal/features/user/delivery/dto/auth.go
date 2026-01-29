0b326d028cf31e1562b05d4c9069356a00b4e571
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-29 17:04:32 +0700

    feat(user): add Roles field to MeResponse for user role information

--- internal/features/user/delivery/dto/auth.go
+++ internal/features/user/delivery/dto/auth.go
@@ -23,4 +23,5 @@ type MeResponse struct {
 	Email          string    `json:"email"`
 	FirstName      string    `json:"first_name"`
 	LastName       string    `json:"last_name"`
+	Roles          []string  `json:"roles"`
 }
