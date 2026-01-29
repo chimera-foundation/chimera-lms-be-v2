b9f3ae46954f60853fd40d503d1ddf7ceff5b972
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-29 14:24:28 +0700

    feat(auth): add helper function GetUserID to retrieve user ID from context

--- internal/shared/auth/const.go
+++ internal/shared/auth/const.go
@@ -1,7 +1,18 @@
 package auth
 
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
 
 type contextKey string
 const (
 	UserIDKey contextKey = "userID"
 )
+
+func GetUserID(ctx context.Context) (uuid.UUID, bool) {
+	userID, ok := ctx.Value(UserIDKey).(uuid.UUID)
+	return userID, ok
+}
