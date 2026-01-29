a58a927d9ce7d5b6ded0857227e6dd09c1f53de0
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-29 21:15:19 +0700

    feat(auth): add OrgIDKey and GetOrgID function for context retrieval

--- internal/shared/auth/const.go
+++ internal/shared/auth/const.go
@@ -10,9 +10,15 @@ import (
 type contextKey string
 const (
 	UserIDKey contextKey = "userID"
+	OrgIDKey contextKey = "orgID"
 )
 
 func GetUserID(ctx context.Context) (uuid.UUID, bool) {
 	userID, ok := ctx.Value(UserIDKey).(uuid.UUID)
 	return userID, ok
+}
+
+func GetOrgID(ctx context.Context) (uuid.UUID, bool) {
+	orgID, ok := ctx.Value(OrgIDKey).(uuid.UUID)
+	return orgID, ok
 }
