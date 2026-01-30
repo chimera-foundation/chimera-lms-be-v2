2d3a9f29005eba4bdb663bd89c769ed500eda558
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-30 16:18:49 +0700

    feat(organization): add OrganizationSeeder for seeding organizations and academic periods

---
+++ internal/features/organization/seed/organization_seeder.go
@@ -0,0 +1,52 @@
+package seed
+
+import (
+	"context"
+	"time"
+
+	"github.com/chimera-foundation/chimera-lms-be-v2/internal/features/organization/domain"
+)
+
+type OrganizationSeeder struct {
+	or domain.OrganizationRepository
+	apr domain.AcademicPeriodRepository
+}
+
+func NewOrganizationSeeder(or domain.OrganizationRepository, apr domain.AcademicPeriodRepository) *OrganizationSeeder {
+	return &OrganizationSeeder{
+		or: or,
+		apr: apr,
+	}
+}
+
+func (s *OrganizationSeeder) SeedOrganizations(ctx context.Context) error {
+	mock_period := domain.NewAcademicPeriod(
+		"2026/2027 genap",
+		time.Date(2026, time.January, 1, 0, 0, 0, 0, time.UTC), // Typical start of Ganjil
+    	time.Date(2026, time.June, 31, 23, 59, 59, 0, time.UTC),
+	)
+
+	org_type := domain.OrgType("high_school")
+
+	organization := domain.NewOrganization(
+		"Candle Tree School",
+		"cts",
+		org_type,
+		"Mars, Planet ke Gaktau",
+		[]domain.AcademicPeriod{*mock_period},
+	)
+
+	organization.PrepareCreate(nil)
+
+	err := s.apr.Create(ctx, mock_period, organization.ID)
+	if err != nil {
+		return err
+	}
+
+	err = s.or.Create(ctx, organization)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
