12c184ce5a9b8bf27e5e4d392b11500f774d996b
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-30 17:30:10 +0700

    feat(seed): add education level seeding and improve organization fetching logs

--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-    "context"
-    "time"
+	"context"
+	"time"
 
-    "github.com/chimera-foundation/chimera-lms-be-v2/internal/app"
-    ur "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/user/repository/postgres"
-    or "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/organization/repository/postgres"
-    u "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/user/seed"
-    o "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/organization/seed"
+	"github.com/chimera-foundation/chimera-lms-be-v2/internal/app"
+	or "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/organization/repository/postgres"
+	o "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/organization/seed"
+	ur "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/user/repository/postgres"
+	u "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/user/seed"
+	elr "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/education_level/repository/postgres"
+	el "github.com/chimera-foundation/chimera-lms-be-v2/internal/features/education_level/seed"
+	"github.com/chimera-foundation/chimera-lms-be-v2/internal/shared/auth"
 )
 
 func main() {
@@ -19,9 +22,13 @@ func main() {
 
     roleRepo := ur.NewRoleRepository(db)
     roleSeeder := u.NewRoleSeeder(roleRepo)
+    
     organizationRepo := or.NewOrganizationRepo(db)
     acadPeriodRepo := or.NewAcademicPeriodRepository(db)
     organizationSeeder := o.NewOrganizationSeeder(organizationRepo, acadPeriodRepo)
+
+    educationLevelRepo := elr.NewEducationLevelRepository(db)
+    educationLevelSeeder := el.NewEducationLevelRepository(educationLevelRepo)
 
     ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
     defer cancel()
@@ -33,11 +40,30 @@ func main() {
     } else {
         logger.Info("Role seeding complete...")
     }
+
     logger.Info("Starting organization and academic period seeding...")
     err = organizationSeeder.SeedOrganizations(ctx)
     if err != nil {
         logger.Info("Organization and academic period seeding failed: ", err.Error())
     } else {
         logger.Info("Organization and academic period seeding complete...")
+    }
+
+    logger.Info("Fetching Organization...")
+    organization, err := organizationRepo.GetBySlug(ctx, "cts")
+    if err != nil {
+        logger.Info("Organization not found")
+    } else {
+        logger.Info("Organization found: ", organization.ID)
+    }
+
+    ctx = context.WithValue(ctx, auth.OrgIDKey, organization.ID)
+
+    logger.Info("Starting education level seeding...")
+    err = educationLevelSeeder.SeedEducationLevels(ctx)
+    if err != nil {
+        logger.Info("Education Level seeding failed: ", err.Error())
+    } else {
+        logger.Info("Education Level seeding complete...")
     }
 }
